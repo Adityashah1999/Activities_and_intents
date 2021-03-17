1bcd62ff4aa34a17095ab73cfdb5af78a30dc8d5
Aditya Shah <nischalshah1999@gmail.com>
2021-03-17 14:15:17 +0545

    Homework completed.

--- app/src/main/java/com/aditya/twoactivities/secondActivity.java
+++ app/src/main/java/com/aditya/twoactivities/secondActivity.java
@@ -8,53 +8,21 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-//public class secondActivity extends AppCompatActivity {
-//
-//    public static final String EXTRA_REPLY =
-//            "com.example.android.twoactivities.extra.REPLY";
-//
-//
-//    private EditText mReply;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_second);
-//
-//
-//        mReply = findViewById(R.id.editText_second);
-//
-//
-//        Intent intent = getIntent();
-//        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-//
-//
-//        TextView textView = findViewById(R.id.text_message);
-//        textView.setText(message);
-//    }
-//
-//
-//    public void returnReply(View view) {
-//
-//        String reply = mReply.getText().toString();
-//
-//
-//        Intent replyIntent = new Intent();
-//        replyIntent.putExtra(EXTRA_REPLY, reply);
-//        setResult(RESULT_OK, replyIntent);
-//        finish();
-//    }
-//}
-
 public class secondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        TextView helloMsg = findViewById(R.id.hello_text);
         Intent intent = getIntent();
         int mCount = intent.getIntExtra(MainActivity.EXTRA_COUNT, 0);
-        TextView mCountTextView = findViewById(R.id.show_count);
+        TextView mCountTextView = findViewById(R.id.count_text);
         mCountTextView.setText(String.valueOf(mCount));
+        mCountTextView.setVisibility(View.VISIBLE);
+        helloMsg.setVisibility(View.VISIBLE);
+
+
+
     }
 }
