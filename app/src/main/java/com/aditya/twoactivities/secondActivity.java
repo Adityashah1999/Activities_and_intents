dbb961e9ce77d48c9e0ac6de94be1a64e5b27b07
Aditya Shah <nischalshah1999@gmail.com>
2021-03-17 12:55:06 +0545

    Homework completed.

--- app/src/main/java/com/aditya/twoactivities/secondActivity.java
+++ app/src/main/java/com/aditya/twoactivities/secondActivity.java
@@ -8,40 +8,53 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+//public class secondActivity extends AppCompatActivity {
+//
+//    public static final String EXTRA_REPLY =
+//            "com.example.android.twoactivities.extra.REPLY";
+//
+//
+//    private EditText mReply;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_second);
+//
+//
+//        mReply = findViewById(R.id.editText_second);
+//
+//
+//        Intent intent = getIntent();
+//        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+//
+//
+//        TextView textView = findViewById(R.id.text_message);
+//        textView.setText(message);
+//    }
+//
+//
+//    public void returnReply(View view) {
+//
+//        String reply = mReply.getText().toString();
+//
+//
+//        Intent replyIntent = new Intent();
+//        replyIntent.putExtra(EXTRA_REPLY, reply);
+//        setResult(RESULT_OK, replyIntent);
+//        finish();
+//    }
+//}
+
 public class secondActivity extends AppCompatActivity {
-
-    public static final String EXTRA_REPLY =
-            "com.example.android.twoactivities.extra.REPLY";
-
-
-    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
-
-        mReply = findViewById(R.id.editText_second);
-
-
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
-    }
-
-
-    public void returnReply(View view) {
-
-        String reply = mReply.getText().toString();
-
-
-        Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_REPLY, reply);
-        setResult(RESULT_OK, replyIntent);
-        finish();
+        int mCount = intent.getIntExtra(MainActivity.EXTRA_COUNT, 0);
+        TextView mCountTextView = findViewById(R.id.show_count);
+        mCountTextView.setText(String.valueOf(mCount));
     }
 }
