e1fe070e84a0cbac61296af10a4ac475348fca87
Aditya Shah <nischalshah1999@gmail.com>
2021-03-17 09:20:41 +0545

    Task4 completed.

--- app/src/main/java/com/aditya/twoactivities/MainActivity.java
+++ app/src/main/java/com/aditya/twoactivities/MainActivity.java
@@ -7,10 +7,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
+    public static final int TEXT_REQUEST = 1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
 
     public static final String EXTRA_MESSAGE = "com.aditya.twoactivities.extra.MESSAGE";
     private EditText mMessageEditText;
@@ -20,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMessageEditText= findViewById(R.id.editText_main);
+        mReplyHeadTextView= findViewById(R.id.text_header_reply);
+        mReplyTextView= findViewById(R.id.text_message_reply);
     }
 
     public void launchSecondActivity(View view) {
@@ -27,6 +33,20 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, secondActivity.class);
         String message = mMessageEditText.getText().toString();
                 intent.putExtra(EXTRA_MESSAGE, message);
-;        startActivity(intent);
+;        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply =
+                        data.getStringExtra(secondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
